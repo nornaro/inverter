2513ce956441d52330bd5e6594c0f713fde62fbc
steve pine <fuyuhimematsuri@gmail.com>
2025-03-19 15:14:37 +0100

    versions

---
+++ tetőtér.gd
@@ -0,0 +1,12 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("pressed",change)
+	pass # Replace with function body.
+
+
+func change():
+	get_tree().call_group("ver","hide")
+	$"../../../ver/tetoter".show()
